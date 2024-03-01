d4f82968d8da989d4d00d0571839428d25f8c842
Trevor Martin <marttrev@mail.gvsu.edu>
2024-02-29 21:21:21 -0500

    Added GameListener for collision detection.

--- include/LEAGUE/physics.h
+++ include/LEAGUE/physics.h
@@ -3,16 +3,19 @@
 
 #include <box2d/box2d.h>
 #include <LEAGUE/common_data_structures.h>
+#include "game_listener.h"
 
 class PhysicsWorld : public Updateable {
 	public:
 		PhysicsWorld(b2Vec2 gravity);
+		~PhysicsWorld();
 		b2Body* addBody(b2BodyDef* bodyDefinition);
 		b2Joint* addJoint(b2JointDef* jointDefinition);
 		void update(double delta);
 	private:
 		b2Vec2 gravity;
 		b2World world;
+		GameListener* listener;
 		int velocityIterations = 6;
 		int positionIterations = 2;
 };
--- src/LEAGUE/physics.cpp
+++ src/LEAGUE/physics.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <LEAGUE/physics.h>
 #include <box2d/box2d.h>
+#include "game_listener.h"
 
 PhysicsWorld::PhysicsWorld(b2Vec2 gravity) : world(gravity){
 	this->gravity = gravity;
 	std::cout << "Gravity: " << world.GetGravity().x << ", " << world.GetGravity().y << std::endl;
+	listener = new GameListener();
+	world.SetContactListener(listener);
+}
+
+PhysicsWorld::~PhysicsWorld() {
+	delete listener;
 }
 
 b2Body* PhysicsWorld::addBody(b2BodyDef* bodyDefinition){
